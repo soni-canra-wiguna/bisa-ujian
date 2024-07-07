060a321a7789a6f137f0764bbb115b51e91ac3b2
soni-canra-wiguna <sonicandra0511@gmail.com>
2024-07-07 16:56:38 +0700

    feat: improve input key

--- components/go-to-exam.tsx
+++ components/go-to-exam.tsx
@@ -3,9 +3,11 @@
 import Link from "next/link"
 import { useEffect, useState } from "react"
 import { Button } from "./ui/button"
-import { ArrowRight } from "lucide-react"
+import { ArrowRightIcon, Copy } from "lucide-react"
 import { Input } from "./ui/input"
 import { ChangeEvent } from "react"
+import CustomTooltip from "./custom-tooltip"
+import { toast } from "./ui/use-toast"
 
 const GoToExam = () => {
   const [url, setUrl] = useState<string>("")
@@ -19,23 +21,44 @@ const GoToExam = () => {
     }
   }, [url])
 
+  const copyExampleKey = () => {
+    navigator.clipboard.writeText("6618e0156e7a54633f88438c")
+    toast({
+      title: "copied example key!",
+    })
+  }
+
   return (
-    <div className="flex items-center sm:max-w-sm lg:max-w-md mx-auto w-full gap-4">
-      <Input
-        value={url}
-        placeholder="masukkan key"
-        onChange={(e: ChangeEvent<HTMLInputElement>) => setUrl(e.target.value)}
-        className="border-x-0 border-t-0 border-b rounded-none focus-visible:ring-0 focus-visible:ring-transparent focus:border-primary p-0"
-      />
-      <Button
-        disabled={disableButton}
-        className="gap-2 capitalize shadow-lg shadow-primary/10 hover:shadow-primary/20 transition-all"
-      >
-        <Link href={`/exam/${url}`} className="flex items-center gap-2">
-          mulai ujian
-          <ArrowRight className="w-4 aspect-square mt-0.5" />
-        </Link>
-      </Button>
+    <div className="flex items-center gap-4 sm:max-w-sm lg:max-w-md mx-auto w-full">
+      <div className="flex items-center gap-4 relative flex-1">
+        <Input
+          value={url}
+          placeholder="masukkan key"
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            setUrl(e.target.value)
+          }
+          className="border-x-0 border-t-0 border-b focus-visible:ring-0 focus-visible:ring-transparent focus:border-primary focus:bg-primary/10 rounded-md pl-4 pr-10 focus:shadow-lg focus:shadow-primary/20"
+        />
+        {
+          !disableButton && <button
+          disabled={disableButton}
+          className="absolute right-0 top-1/2 -translate-y-1/2 w-max rounded-md p-2 hover:bg-primary/40 transition-all duration-300 disabled:cursor-not-allowed"
+        >
+          <Link href={`/exam/${url}`} className="flex items-center gap-2">
+            <ArrowRightIcon className="size-4 text-foreground" />
+          </Link>
+        </button>
+        }
+      </div>
+      <CustomTooltip message="copy example key">
+        <Button
+          onClick={copyExampleKey}
+          className="flex items-center justify-center bg-transparent hover:bg-primary/10 border hover:border-primary/40 group transition-all duration-300"
+          size="icon"
+        >
+          <Copy className="size-4 text-foreground group-hover:text-primary transition-all duration-300" />
+        </Button>
+      </CustomTooltip>
     </div>
   )
 }
